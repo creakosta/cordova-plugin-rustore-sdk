dc77fca06dc7556266e1a5c0ddf20d2fc7c003b7
creakosta <creakosta@mail.ru>
2023-04-25 09:58:00 +0300

    Add files via upload

--- src/android/RuStorePlugin.kt
+++ src/android/RuStorePlugin.kt
@@ -206,8 +206,8 @@ class RuStorePlugin : CordovaPlugin() {
   * @param callbackContext The callback context used when calling back into JS code
   */
   private fun getProducts(args: JSONArray, callbackContext: CallbackContext) {
-	  val productIds = List<String>(args.length()) {
-		args.getString(it)
+	  val productIds = if(args.length()) List<String>(args.length()) {
+		args.getString(it) else null
 	  }
 	  
 	  RuStoreBillingClient.products.getProducts(productIds)
